c946bc71bd404f50dfcfd57630d57ee0ca87f726
YangKunlun <yangkunlun@gmail.com>
2019-01-10 21:45:28 +0800

    refactor the test interface!

--- conv/main.c
+++ conv/main.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <getopt.h>
 #include <emmintrin.h>
+#include "common.h"
 
 void conv_basic(const float * restrict a, int n, const float * restrict f, int m, float * restrict b)
 {
@@ -24,6 +26,15 @@ void data_gen1(float *a, int n, float *f, int m)
         a[i] = i+1;
     for(int i=0; i<m; i++)
         f[i] = 2*(i+1);
+}
+
+/**
+ * Generate random data in range [-10, 10]
+ * */
+void data_gen_rand(float *a, int n)
+{
+    for(int i=0; i<n; i++)
+        a[i] = 10.0f*(0.5f-random()*1.0f/RAND_MAX);
 }
 
 void conv_optimize(const float * restrict a, int n, const float * restrict f, int m, float * restrict b)
@@ -68,7 +79,8 @@ void conv_opsse2(const float * restrict a, int n, const float * restrict f, int 
             fi[0] = _mm_load_ps1(f+4*0+j);
             fi[1] = _mm_load_ps1(f+4*1+j);
             fi[2] = _mm_load_ps1(f+4*2+j);
-            for(int i=0*4; i<2*4; i+=4) {
+            for(int i=0*4; i<2*4; i+=4) {   //this is not correct!
+                //valgrind will not pass!!!!
                 bi = _mm_loadu_ps(b+i);
                 ai = _mm_loadu_ps(a+i-4*0-j);
                 bi = _mm_add_ps(bi, _mm_mul_ps(ai, fi[0]));
@@ -100,33 +112,117 @@ void conv_opdeci(const float * restrict a, int n, const float * restrict f, int 
             b[i/2] += f[j]*a[i-j];
 }
 
+void (*algorithm_func[])(const float*, int, const float*, int, float*)
+    = { &conv_basic, 
+        &conv_optimize
+    };
 
-int main()
+int fir_valid(int n, int m, int algorithm_id, int verbose)
 {
-    int repeat=100000;
-    //int repeat=1;
-    int n=4096, m=12;
-    float a[n], b[n], c[n], d[n], f[m];
-
-    data_gen1(a, n, f, m);
-    memset(b, 0, n*sizeof(float));
-    memset(c, 0, n*sizeof(float));
-
-    for(int i=0; i<repeat; i++) {
-        if(i%1000==0)
-            printf("run@%d\n", i);
-        //conv_basic(a, n, f, m, b);
-        //conv_decimate(a, n, f, m, b);
-        //conv_optimize(a, n, f, m, c);
-        conv_opsse2(a, n, f, m, c);
-        //conv_opdeci(a, n, f, m, c);
-    }
-    memset(b, 0, n*sizeof(float));
+    float *a, *f, *b, *c;
+    a = calloc(n, sizeof(float));
+    f = calloc(m, sizeof(float));
+    b = calloc(n, sizeof(float));
+    c = calloc(n, sizeof(float));
+    data_gen_rand(a, n);
+    data_gen_rand(f, m);
     conv_basic(a, n, f, m, b);
-    for(int i=0; i<n; i++) {
-        if(fabs(b[i]-c[i])>0.1)
-            printf("%4d b=%e c=%e\n", i, b[i], c[i]);
-    }
+    algorithm_func[algorithm_id](a, n, f, m, c);
 
+    int bad_points=0;
+    for(int i=0; i<n; i++)
+        if(fabs(b[i]-c[i])>0.01f) {
+            bad_points++;
+            if(verbose)
+                printf("%4d b=%e c=%e\n", i, b[i], c[i]);
+        }
+    free(a);
+    free(f);
+    free(b);
+    free(c);
+    return bad_points;
+}
+
+void fir_test(int n, int m, int repeat, int algorithm_id, int check, int verbose)
+{
+    float *a, *f, *b;
+    a = calloc(n, sizeof(float));
+    f = calloc(m, sizeof(float));
+    b = calloc(n, sizeof(float));
+
+    clock_t start = clock();
+    for(int i=0; i<repeat; i++) {
+        algorithm_func[algorithm_id](a, n, f, m, b);
+        asm("");
+    }
+    clock_t current=clock();
+    free(a);
+    free(f);
+    free(b);
+
+    float tcost = (current-start)*1.0f/CLOCKS_PER_SEC;
+    printf("%2d %8d %4d %8d %9.4f\n", algorithm_id, n, m, repeat, tcost);
+}
+/**
+ *  -a algrithm id
+ *  -m 12
+ *  -n 4096
+ *  -r 100000
+ *  -c check correctness
+ *  -v vorbose
+ * */
+int main(int argc, char **argv)
+{
+    char ch; int debug=0;
+    int m=0, n=0, repeat=0, check=0, verbose=0, algorithm_id=0;
+    while((ch=getopt(argc, argv, "a:m:n:r:cv"))!=-1)
+        switch(ch) {
+            case 'a':
+                if(debug) { printf("-a %s\n", optarg);}
+                algorithm_id = atoi(optarg);
+                break;
+            case 'm':
+                if(debug) { printf("-m %s\n", optarg);}
+                m = atoi(optarg);
+                break;
+            case 'n':
+                if(debug) { printf("-n %s\n", optarg);}
+                n = atoi(optarg);
+                break;
+            case 'r':
+                printf("-r %s\n", optarg);
+                repeat = atoi(optarg);
+                break;
+            case 'c':
+                printf("-c\n");
+                check = 1;
+                break;
+            case 'v':
+                printf("-v\n");
+                verbose = 1;
+                break;
+            case '?':
+                printf("Usage: %s -n -m -r -a [-c -v]\n"
+                        "\t -n : input series length\n"
+                        "\t -m : short filter length\n"
+                        "\t -r : repeat times\n"
+                        "\t -a : algorithm id\n"
+                        "\t -c : check algorithm against basic one\n"
+                        "\t -v : verbose print\n", argv[0]);
+                break;
+            default:
+                abort();
+        }
+
+    if(fir_valid(n,m,algorithm_id, verbose)) {
+        printf("fir_valid!\n");
+        if(verbose)
+            printf("Failed algorithm=%d, result is wrong!\n", algorithm_id);
+        exit(1);
+    }
+    if(verbose)
+        printf("algorithm_id=%d successful!\n", algorithm_id);
+
+    fir_test(n, m, repeat, algorithm_id, check, verbose);
     return 0;
 }
--- conv/make.sh
+++ conv/make.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-gcc -o basic -DOP1 main.c -O3 -march=native
+flag="-O3 -march=native -W -Wall -std=gnu99"
+gcc -c $flag common.c
+gcc -o basic $flag main.c common.o -lm
+
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+./basic -n 4096 -m 12 -r 100000 -a 0 -c -v
